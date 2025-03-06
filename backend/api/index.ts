51e39a8ba910a6ccc43e8e71d463fc92d3fce94d
javonjax <javonj461@hotmail.com>
2025-03-06 00:49:49 -0500

    Updated cors settings.

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -15,7 +15,7 @@ const NODE_ENV = process.env.NODE_ENV || 'development';
 app.use(express.json());
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://astrolens-omega.vercel.app/'],
+    origin: ['http://localhost:5173', 'https://astrolens-omega.vercel.app'],
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
   }),
